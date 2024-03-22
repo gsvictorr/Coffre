fc3efd55b0bf4f6ba696a06e5cf07a7bf5047d77
Victor Silva <vitinhogsoo@gmail.com>
2024-03-22 11:18:04 -0300

    adjusts

--- coffre-backend/src/main/java/br/com/coffre/Backend.java
+++ coffre-backend/src/main/java/br/com/coffre/Backend.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoffreApplication {
+public class Backend {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoffreApplication.class, args);
+		SpringApplication.run(Backend.class, args);
 	}
 
 }
