bb536ee5b3084abb12eec39079e61d152c7e5d37
Victor Silva <vitinhogsoo@gmail.com>
2024-03-22 11:01:47 -0300

    initial commit

---
+++ coffre-backend/src/main/java/br/com/coffre/CoffreApplication.java
@@ -0,0 +1,13 @@
+package br.com.coffre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffreApplication.class, args);
+	}
+
+}
