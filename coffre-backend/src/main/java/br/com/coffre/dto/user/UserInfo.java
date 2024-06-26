5dbac34fc35c317ffeb3c25cb999ffb4733ecfae
Victor Silva <vitinhogsoo@gmail.com>
2024-06-26 20:20:38 -0300

    fix: bug userinfo

--- coffre-backend/src/main/java/br/com/coffre/dto/user/UserInfo.java
+++ coffre-backend/src/main/java/br/com/coffre/dto/user/UserInfo.java
@@ -2,11 +2,12 @@ package br.com.coffre.dto.user;
 
 
 
+import br.com.coffre.config.role.UserRole;
 import br.com.coffre.model.User;
 
-public record UserInfo (Long id, String name, String email){
+public record UserInfo (Long id, String name, String email, UserRole userRole){
     
     public UserInfo(User user){
-        this(user.getId(), user.getName(), user.getEmail());
+        this(user.getId(), user.getName(), user.getEmail(), user.getRole());
     }
 }
